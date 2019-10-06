f796a027c468e55c6b03bcaf5415809399e67efc
Mayur-Nandu <nandumayur95@gmail.com>
2019-10-06 22:31:24 +0530

    working read,clear and highlight

--- content.js
+++ content.js
@@ -1,17 +1,34 @@
 chrome.runtime.onMessage.addListener(highlightSelection);
 
 function highlightSelection(request,sender,sendResponse){
-    var x=window.getSelection();
-    ele= document.createElement('span');
-    
-    ele.setAttribute("style", "background-color:yellow");
-    if(x.focusNode){ 
-        console.log(x.toString());
-        range= x.getRangeAt(0);
-        var documentFragment = range.extractContents();
-        ele.appendChild(documentFragment);
-        range.insertNode(ele);
-        window.localStorage.setItem(request.page_url,document.getElementsByTagName("body")[0].innerHTML);
+    if(request.clr==1)
+    {
+        localStorage.removeItem(request.page_url);
     }
+    else if(request.ReadHighlight == 0){
+        //perform function for highloghting
+        var x=window.getSelection();
+        ele= document.createElement('span');
+        
+        ele.setAttribute("style", "background-color:yellow");
+        if(x.focusNode){ 
+            console.log(x.toString());
+            range= x.getRangeAt(0);
+            var documentFragment = range.extractContents();
+            ele.appendChild(documentFragment);
+            range.insertNode(ele);
+            window.localStorage.setItem(request.page_url,document.getElementsByTagName("body")[0].innerHTML);
+        }
+    }
+    else if(request.ReadHighlight == 1)
+    {
+        //perform operation for retriving data from localstorage
+        page_html=localStorage.getItem(request.page_url);
+        if(page_html){
+            document.getElementsByTagName("body")[0].innerHTML=page_html;
+        }
+    }
+    
+   
 }
 
--- popup.js
+++ popup.js
@@ -1,31 +1,37 @@
 document.getElementById("ReadHighlight").addEventListener('click',()=>{
-  var b2=localStorage.getItem("body1");
-  console.log(typeof(b2));
-  if(b2){
-    document.getElementsByTagName("body")[0].innerHTML=b2;
-  }
   
+  let param={
+    active:true, //get all the active tabs 
+    currentWindow:true //get the current tab 
+  }
+  chrome.tabs.query(param,(tabs)=>
+  {
+    // var b2=localStorage.getItem("https://en.wikipedia.org/wiki/Main_Page");
+      let msg={
+        page_url:tabs[0].url,
+        ReadHighlight:1,
+        clr:0
+      }
+      chrome.tabs.sendMessage(tabs[0].id,msg);
+  });
 });
 
-document.getElementById("btn2").addEventListener('click',()=>{
-  var b2=localStorage.removeItem("body1");
+document.getElementById("clearHighlight").addEventListener('click',()=>{
+  let param={
+    active:true, //get all the active tabs 
+    currentWindow:true //get the current tab 
+  }
+  chrome.tabs.query(param,(tabs)=>
+  {
+    // var b2=localStorage.getItem("https://en.wikipedia.org/wiki/Main_Page");
+      let msg={
+        page_url:tabs[0].url,
+        ReadHighlight:1,
+        clr:1
+      }
+      chrome.tabs.sendMessage(tabs[0].id,msg);
+  });
 });
-
-
-// document.getElementById('btn').addEventListener('click',()=>{
-//   var x=window.getSelection();
-//   ele= document.createElement('span');
-//   ele.setAttribute("style", "background-color:yellow");
-//   if(x.focusNode){ 
-//     // console.log(x.toString()); 
-//     range= x.getRangeAt(0);
-//     var documentFragment = range.extractContents();
-//     ele.appendChild(documentFragment);
-//     range.insertNode(ele);
-//     window.localStorage.setItem("body1",document.getElementsByTagName("body")[0].innerHTML);
-//   }
-// });
-   
 
 document.getElementById('highlight').addEventListener('click',sendMsg);
 
@@ -39,7 +45,9 @@ function sendMsg()
   {
     console.log(tabs)
       let msg={
-        page_url:tabs[0].url
+        page_url:tabs[0].url,
+        ReadHighlight:0,
+        clr:0
       }
       chrome.tabs.sendMessage(tabs[0].id,msg);
   });
